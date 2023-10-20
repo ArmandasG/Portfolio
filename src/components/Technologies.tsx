498a18f5c83b40ba0736a030b1826977f0cf1c4d
Armandas G <genysarmandas@gmail.com>
2023-10-20 15:30:15 +0300

    Technologies: ScreenSizing

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -5,7 +5,7 @@ import SingleTechnology from './SingleTechnology'
 function Technologies() {
   return (
     <div id='technologies' className='w-full lg:h-screen'>
-        <div className='max-w-[1240px] sm:m-auto flex flex-col justify-center h-full mx-2'>
+        <div className='max-w-[1240px] lg:m-auto flex flex-col justify-center h-full mx-2'>
         <p className='text-xl tracking-widest uppercase'>Technologies</p>
         <h2 className='py-4'>What I Can Do</h2>
         <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-0 md:gap-8'>
