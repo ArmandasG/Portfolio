b2219f2d8d68bda9b078499fb5787582534b3034
Armandas G <genysarmandas@gmail.com>
2023-11-10 21:38:32 +0200

    possible feature

--- src/functionalities/RevealOnScroll.ts
+++ src/functionalities/RevealOnScroll.ts
@@ -1,18 +1,25 @@
-export function reveal() {
-    let allReveal = document.querySelectorAll('.reveal');
-
-    for(let i = 0; i < allReveal.length; i++ ) {
-        let windowHeight = window.innerHeight;
-        let revealTop = allReveal[i].getBoundingClientRect().top;
-        let revealPoint = 150;
-
-        if (revealTop < windowHeight - revealPoint) {
-            allReveal[i].classList.add('active');
-        } 
-        
-        // else {
-        //     allReveal[i].classList.remove('acitve')
-        // }
+function revealOnScroll() {
+    if (typeof window !== 'undefined') {
+        reveal()
     }
+  }
 
+  function reveal() {
+  if (typeof window !== 'undefined') {
+  let allReveal = document.querySelectorAll('.reveal');
+
+  for(let i = 0; i < allReveal.length; i++ ) {
+      let windowHeight = window.innerHeight;
+      let revealTop = allReveal[i].getBoundingClientRect().top;
+      let revealPoint = 150;
+
+      if (revealTop < windowHeight - revealPoint) {
+          allReveal[i].classList.add('active');
+      } 
+      
+      // else {
+      //     allReveal[i].classList.remove('acitve')
+      // }
+  }
+  }
 }
